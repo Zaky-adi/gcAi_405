16357f1c7fa2a1883519b9971a201167fbd0733a
Zaky Adi Putra <putrazakyadi@gmail.com>
2026-04-16 06:31:52 +0700

    hosting ke vercel

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -51,5 +51,8 @@ $app->singleton(
 | from the actual running of the application and sending responses.
 |
 */
+if (isset($_ENV['VERCEL_ENV'])) {
+    $app->useStoragePath('/tmp');
+}
 
 return $app;
