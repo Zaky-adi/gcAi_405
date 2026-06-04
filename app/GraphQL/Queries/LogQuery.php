06fbdb4af01b7f079aaaea453ee3a1abb7c96502
Zaky Adi Putra <putrazakyadi@gmail.com>
2026-06-05 01:06:55 +0700

    ADD: endpoint jumlah kendaraan masuk

---
+++ app/GraphQL/Queries/LogQuery.php
@@ -0,0 +1,12 @@
+<?php declare(strict_types=1);
+
+namespace App\GraphQL\Queries;
+
+final readonly class LogQuery
+{
+    /** @param  array{}  $args */
+    public function resolve(null $_, array $args)
+    {
+        // TODO implement the resolver
+    }
+}
